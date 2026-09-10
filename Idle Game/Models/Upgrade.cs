f45396db4007a2af0d98c5e7c0682fe806baffc7
SemOtto <D310596@edu.curio.nl>
2026-09-10 11:55:35 +0200

    Upgrade Model aangepast
    Deze model slaat alles op van de upgrades die je aanmaakt hoeveel het kost de naam dat soort dingen.

---
+++ Idle Game/Models/Upgrade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idle_Game.Models;
+
+public class Upgrade
+{
+    public string Id { get; set; } = "";
+
+    public string Name { get; set; } = "";
+
+    public string Description { get; set; } = "";
+
+    public double Price { get; set; }
+
+    public Upgrade()
+    {
+    }
+
+    public Upgrade(
+        string id,
+        string name,
+        string description,
+        double price)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Price = price;
+    }
+}
