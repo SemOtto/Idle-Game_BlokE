07185789d4a11e15adee88ea93a35ffb34c3a40e
SemOtto <D310596@edu.curio.nl>
2026-09-10 11:05:47 +0200

    PlayerData bestand aangemaakt
    PlayerData aangemaakt in deze model staat de informatie voor de Speler

---
+++ Idle Game/Models/PlayersData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceTycoon.Models;
+
+public class PlayerData
+{
+    public double Credits { get; set; } = 0;
+
+    public double ManualPower { get; set; } = 10;
+
+    public int GalaxyPoints { get; set; } = 0;
+
+    public DateTime LastSaved { get; set; } = DateTime.Now;
+
+    public Dictionary<string, int> Buildings { get; set; } = new();
+
+    public List<string> PurchasedUpgrades { get; set; } = new();
+}
