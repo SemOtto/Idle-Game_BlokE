c35cf6dbbd974a1c70fd98f970c8914002a20cb5
SemOtto <D310596@edu.curio.nl>
2026-09-10 11:07:42 +0200

    Building model aangemaakt
    In deze model staat alle data voor de gebouwen die je in de game kan kopen, Naam de prijs om te kopen en de inkomste per seconde.

---
+++ Idle Game/Models/Building.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceTycoon.Models;
+
+public class Building
+{
+    public string Id { get; set; } = "";
+
+    public string Name { get; set; } = "";
+
+    public string Description { get; set; } = "";
+
+    public double BasePrice { get; set; }
+
+    public double IncomePerSecond { get; set; }
+
+    public Building()
+    {
+    }
+
+    public Building(
+        string id,
+        string name,
+        string description,
+        double basePrice,
+        double incomePerSecond)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        BasePrice = basePrice;
+        IncomePerSecond = incomePerSecond;
+    }
+
+    public double GetPrice(int amount)
+    {
+        return BasePrice * Math.Pow(1.15, amount);
+    }
+}
